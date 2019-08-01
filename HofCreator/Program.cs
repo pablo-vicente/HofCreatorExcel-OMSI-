d8ef47d10a0d5384c85623ba69c03cb25d23aebe
Pablo Vicente <pablo.vicente@outlook.com.br>
2019-08-01 00:24:11 -0300

    Add bustops

--- HofCreator/Program.cs
+++ HofCreator/Program.cs
@@ -1,6 +1,8 @@
 ﻿using OfficeOpenXml;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace HofCreator
@@ -12,19 +14,53 @@ namespace HofCreator
             string path = @"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Linhas Palhocity.xlsx";
             FileInfo file = new FileInfo(path);
             ExcelPackage package = new ExcelPackage(file);
-            ExcelWorksheet worksheet = package.Workbook.Worksheets["Palhocity"];
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(AdicionarInformacoesMapa(package));
+            sb.Append(AdicionarTerminus(package));
+            sb.Append(AdicionarBusStops(package));
+
+            File.WriteAllText(@"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Palhocity.hof", sb.ToString());
+        }
+
+        public static StringBuilder AdicionarInformacoesMapa(ExcelPackage package)
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+            int rows = worksheet.Dimension.Rows;
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}\n", "#####TEMPLATE FILE#####");
+            sb.AppendFormat("{0}\n\n\n", "SD200_SD202_Template.xml");
+            sb.AppendFormat("{0}\n", "[name]");
+            sb.AppendFormat("{0}\n\n\n", worksheet.Cells["C3"].Value.ToString());
+            sb.AppendFormat("{0}\n", "[servicetrip]");
+            sb.AppendFormat("{0}\n\n\n", worksheet.Cells["C4"].Value.ToString());
+            sb.AppendFormat("{0}\n", "stringcount_terminus");
+            sb.AppendFormat("{0}\n\n\n", "6");
+            sb.AppendFormat("{0}\n", "stringcount_busstop");
+            sb.AppendFormat("{0}\n\n\n", "4");
+
+            return sb;
+        }
+
+        public static StringBuilder AdicionarTerminus(ExcelPackage package)
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
             int rows = worksheet.Dimension.Rows;
 
             StringBuilder sd = new StringBuilder();
 
             sd.AppendFormat("{0}\n", "===========================================================");
-            sd.AppendFormat("{0}\n", "TERMINI BUS");
+            sd.AppendFormat("{0}\n", "======================= TERMINI BUS =======================");
             sd.AppendFormat("{0}\n", "===========================================================");
 
             for (int i = 2; i <= rows; i++)
             {
-                if (worksheet.Cells[$"A{i}"].Value == null) break;
-                sd.AppendFormat("{0}\n", "[addterminus]");
+                var linebus = worksheet.Cells[$"A{i}"].Value;
+                if (linebus == null) break; ;
+
+                sd.AppendFormat("{0}\n", linebus.ToString().Equals("0") ? "[addterminus_allexit]" : "[addterminus]");
+
                 sd.AppendFormat("{0}\n", worksheet.Cells[$"J{i}"].Value == null ? "" : worksheet.Cells[$"J{i}"].Value.ToString());
                 sd.AppendFormat("{0}\n", worksheet.Cells[$"C{i}"].Value == null ? "" : worksheet.Cells[$"C{i}"].Value.ToString());
                 sd.AppendFormat("{0}\n", worksheet.Cells[$"G{i}"].Value == null ? "" : worksheet.Cells[$"G{i}"].Value.ToString());
@@ -34,9 +70,47 @@ namespace HofCreator
                 sd.AppendFormat("{0}\n", worksheet.Cells[$"I{i}"].Value == null ? "" : worksheet.Cells[$"I{i}"].Value.ToString());
                 sd.AppendFormat("{0}\n", worksheet.Cells[$"H{i}"].Value == null ? "" : worksheet.Cells[$"H{i}"].Value.ToString());
                 sd.AppendFormat("{0}\n", "-----------------------------------------------------------");
-
             }
-            File.WriteAllText(@"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Palhocity.hof", sd.ToString());
+            return sd;
+        }
+
+        public static StringBuilder AdicionarBusStops(ExcelPackage package)
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+            int rows = worksheet.Dimension.Rows;
+
+            StringBuilder sd = new StringBuilder();
+
+            sd.AppendFormat("\n\n\n{0}\n", "===========================================================");
+            sd.AppendFormat("{0}\n", "======================== BUS STOPS ========================");
+            sd.AppendFormat("{0}\n", "===========================================================");
+
+            List<string> busStops = new List<string>();
+
+            for (int i = 2; i <= rows; i++)
+            {
+                var linebus = worksheet.Cells[$"A{i}"].Value;
+                if (linebus == null) break; ;
+
+                if (!linebus.ToString().Equals("0"))
+                {
+                    string firtBusStop = worksheet.Cells[$"P{i}"].Value == null ? "" : worksheet.Cells[$"P{i}"].Value.ToString();
+                    string lastBusStop = worksheet.Cells[$"Q{i}"].Value == null ? "" : worksheet.Cells[$"Q{i}"].Value.ToString();
+
+                    busStops.Add(firtBusStop);
+                    busStops.Add(lastBusStop);
+                }
+            }
+            foreach (string busstop in busStops.Distinct().OrderBy(x => x.ToString()))
+            {
+                sd.AppendFormat("{0}\n", "[addbusstop]");
+                for (int j = 1; j <= 5; j++)
+                {
+                    sd.AppendFormat("{0}\n", busstop);
+                }
+                sd.AppendFormat("{0}\n", "-----------------------------------------------------------");
+            }
+            return sd;
         }
     }
 }
