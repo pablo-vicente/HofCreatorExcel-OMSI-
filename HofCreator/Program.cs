b525bf3b929e7bad760359579064c803fdbc0537
Pablo Vicente <pablo.vicente@outlook.com.br>
2019-08-02 23:48:40 -0300

    Fix CRLF bus

--- HofCreator/Program.cs
+++ HofCreator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,14 @@ namespace HofCreator
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Linhas Palhocity.xlsx";
+            Console.WriteLine("Insira o caminho para o Arquivo:");
+            string path = Console.ReadLine();
+            Console.WriteLine("Insira o caminho para salvar o HOF:");
+            string path2 = Console.ReadLine();
+            path2 = @"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity";
+
+
+            path = @"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Linhas Palhocity.xlsx";
             FileInfo file = new FileInfo(path);
             ExcelPackage package = new ExcelPackage(file);
 
@@ -20,8 +28,8 @@ namespace HofCreator
             sb.Append(AdicionarTerminus(package));
             sb.Append(AdicionarBusStops(package));
             sb.Append(AdicionarTripsRoutas(package));
-
-            File.WriteAllText(@"C:\Users\pablo\OneDrive\1-Documentos\2 - Projetos\3 - Map Palhocity\Palhocity.hof", sb.ToString());
+            File.WriteAllText(Path.Combine(path2, "Palhocity.hof"), sb.ToString(), Encoding.Default);
+            Process.Start("explorer.exe", path2);
         }
 
         public static StringBuilder AdicionarInformacoesMapa(ExcelPackage package)
@@ -30,16 +38,16 @@ namespace HofCreator
             int rows = worksheet.Dimension.Rows;
 
             StringBuilder sb = new StringBuilder();
-            sb.AppendFormat("{0}\n", "#####TEMPLATE FILE#####");
-            sb.AppendFormat("{0}\n\n\n", "SD200_SD202_Template.xml");
-            sb.AppendFormat("{0}\n", "[name]");
-            sb.AppendFormat("{0}\n\n\n", worksheet.Cells["C3"].Value.ToString());
-            sb.AppendFormat("{0}\n", "[servicetrip]");
-            sb.AppendFormat("{0}\n\n\n", worksheet.Cells["C4"].Value.ToString());
-            sb.AppendFormat("{0}\n", "stringcount_terminus");
-            sb.AppendFormat("{0}\n\n\n", "6");
-            sb.AppendFormat("{0}\n", "stringcount_busstop");
-            sb.AppendFormat("{0}\n\n\n", "4");
+            sb.AppendFormat("{0}\r\n", "#####TEMPLATE FILE#####");
+            sb.AppendFormat("{0}\r\n\n\n", "SD200_SD202_Template.xml");
+            sb.AppendFormat("{0}\r\n", "[name]");
+            sb.AppendFormat("{0}\r\n\n\n", worksheet.Cells["C3"].Value.ToString());
+            sb.AppendFormat("{0}\r\n", "[servicetrip]");
+            sb.AppendFormat("{0}\r\n\n\n", worksheet.Cells["C4"].Value.ToString());
+            sb.AppendFormat("{0}\r\n", "stringcount_terminus");
+            sb.AppendFormat("{0}\r\n\n\n", "6");
+            sb.AppendFormat("{0}\r\n", "stringcount_busstop");
+            sb.AppendFormat("{0}\r\n\n\n", "4");
 
             return sb;
         }
@@ -51,26 +59,26 @@ namespace HofCreator
 
             StringBuilder sd = new StringBuilder();
 
-            sd.AppendFormat("{0}\n", "===========================================================");
-            sd.AppendFormat("{0}\n", "======================= TERMINI BUS =======================");
-            sd.AppendFormat("{0}\n", "===========================================================");
+            sd.AppendFormat("{0}\r\n", "===========================================================");
+            sd.AppendFormat("{0}\r\n", "======================= TERMINI BUS =======================");
+            sd.AppendFormat("{0}\r\n\n", "===========================================================");
 
             for (int i = 2; i <= rows; i++)
             {
                 var linebus = worksheet.Cells[$"A{i}"].Value;
                 if (linebus == null) break; ;
 
-                sd.AppendFormat("{0}\n", linebus.ToString().Equals("0") ? "[addterminus_allexit]" : "[addterminus]");
+                sd.AppendFormat("{0}\r\n", linebus.ToString().Equals("0") ? "[addterminus_allexit]" : "[addterminus]");
 
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"J{i}"].Value == null ? "" : worksheet.Cells[$"J{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"C{i}"].Value == null ? "" : worksheet.Cells[$"C{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"G{i}"].Value == null ? "" : worksheet.Cells[$"G{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"D{i}"].Value == null ? "" : worksheet.Cells[$"D{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"E{i}"].Value == null ? "" : worksheet.Cells[$"E{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"F{i}"].Value == null ? "" : worksheet.Cells[$"F{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"I{i}"].Value == null ? "" : worksheet.Cells[$"I{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", worksheet.Cells[$"H{i}"].Value == null ? "" : worksheet.Cells[$"H{i}"].Value.ToString());
-                sd.AppendFormat("{0}\n", "-----------------------------------------------------------");
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"J{i}"].Value == null ? "" : worksheet.Cells[$"J{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"C{i}"].Value == null ? "" : worksheet.Cells[$"C{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"G{i}"].Value == null ? "" : worksheet.Cells[$"G{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"D{i}"].Value == null ? "" : worksheet.Cells[$"D{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"E{i}"].Value == null ? "" : worksheet.Cells[$"E{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"F{i}"].Value == null ? "" : worksheet.Cells[$"F{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"I{i}"].Value == null ? "" : worksheet.Cells[$"I{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", worksheet.Cells[$"H{i}"].Value == null ? "" : worksheet.Cells[$"H{i}"].Value.ToString());
+                sd.AppendFormat("{0}\r\n", "-----------------------------------------------------------");
             }
             return sd;
         }
@@ -82,9 +90,9 @@ namespace HofCreator
 
             StringBuilder sd = new StringBuilder();
 
-            sd.AppendFormat("\n\n\n{0}\n", "===========================================================");
-            sd.AppendFormat("{0}\n", "======================== BUS STOPS ========================");
-            sd.AppendFormat("{0}\n", "===========================================================");
+            sd.AppendFormat("\r\n{0}\r\n", "===========================================================");
+            sd.AppendFormat("{0}\r\n", "======================== BUS STOPS ========================");
+            sd.AppendFormat("{0}\r\n\n", "===========================================================");
 
             List<string> busStops = new List<string>();
 
@@ -104,12 +112,12 @@ namespace HofCreator
             }
             foreach (string busstop in busStops.Distinct().OrderBy(x => x.ToString()))
             {
-                sd.AppendFormat("{0}\n", "[addbusstop]");
+                sd.AppendFormat("{0}\r\n", "[addbusstop]");
                 for (int j = 1; j <= 5; j++)
                 {
-                    sd.AppendFormat("{0}\n", busstop);
+                    sd.AppendFormat("{0}\r\n", busstop);
                 }
-                sd.AppendFormat("{0}\n", "-----------------------------------------------------------");
+                sd.AppendFormat("{0}\r\n", "-----------------------------------------------------------");
             }
             return sd;
         }
@@ -121,9 +129,9 @@ namespace HofCreator
 
             StringBuilder sd = new StringBuilder();
 
-            sd.AppendFormat("\n\n\n{0}\n", "===========================================================");
-            sd.AppendFormat("{0}\n", "========================= ROUTES ==========================");
-            sd.AppendFormat("{0}\n", "===========================================================");
+            sd.AppendFormat("\r\n{0}\r\n", "===========================================================");
+            sd.AppendFormat("{0}\r\n", "========================= ROUTES ==========================");
+            sd.AppendFormat("{0}\r\n\n", "===========================================================");
 
             for (int i = 2; i <= rows; i++)
             {
@@ -137,17 +145,18 @@ namespace HofCreator
                     var nLine = linebus.ToString().Replace("-", "");
                     var nRoute = $"{0}{worksheet.Cells[$"B{i}"].Value.ToString()}";
 
-                    sd.AppendFormat("{0}\n", $"Line {nLine} Route {nRoute} : {firtBusStop} => {lastBusStop}");
-                    sd.AppendFormat("{0}\n\n", "-----------------------------------------------------------");
-                    sd.AppendFormat("{0}\n", "[infosystem_trip]");
-                    sd.AppendFormat("{0}\n", $"{nLine}{nRoute}");
-                    sd.AppendFormat("{0}\n", $"{firtBusStop} => {lastBusStop} 1");
-                    sd.AppendFormat("{0}\n", worksheet.Cells[$"J{i}"].Value == null ? "" : worksheet.Cells[$"J{i}"].Value.ToString());
-                    sd.AppendFormat("{0}\n\n", "via");
-                    sd.AppendFormat("{0}\n", "[infosystem_busstop_list]");
-                    sd.AppendFormat("{0}\n", "2");
-                    sd.AppendFormat("{0}\n", firtBusStop);
-                    sd.AppendFormat("{0}\n\n", lastBusStop);
+                    sd.AppendFormat("{0}\r\n", "-----------------------------------------------------------");
+                    sd.AppendFormat("{0}\r\n", $"Line {nLine} Route {nRoute} : {firtBusStop} => {lastBusStop}");
+                    sd.AppendFormat("{0}\r\n", "-----------------------------------------------------------");
+                    sd.AppendFormat("{0}\r\n", "[infosystem_trip]");
+                    sd.AppendFormat("{0}\r\n", $"{nLine}{nRoute}");
+                    sd.AppendFormat("{0}\r\n", $"{firtBusStop} => {lastBusStop} 1");
+                    sd.AppendFormat("{0}\r\n", worksheet.Cells[$"J{i}"].Value == null ? "" : worksheet.Cells[$"J{i}"].Value.ToString());
+                    sd.AppendFormat("{0}\r\n\n", "via");
+                    sd.AppendFormat("{0}\r\n", "[infosystem_busstop_list]");
+                    sd.AppendFormat("{0}\r\n", "2");
+                    sd.AppendFormat("{0}\r\n", firtBusStop);
+                    sd.AppendFormat("{0}\r\n", lastBusStop);
                 }
             }
             return sd;
